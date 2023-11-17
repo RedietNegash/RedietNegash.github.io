1fb72fda97bcfd014ebd332f8c9016a77606c8e5
RedietNegash <redietnegash9@gmail.com>
2023-11-17 22:22:32 +0300

    fix: updated vite config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Explicitly set MIME type for JSX files
-    fs: {
-      strict: false,
-    },
-  },
   base: "/RedietNegash.github.io/",
 });
